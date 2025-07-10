868dc75c6b180f0b70ad7c4bad85e98aa8896bee
Haitham Magdy <hmagdyadel@gmail.com>
2025-07-10 15:38:16 +0300

    work on prevent screen shot

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,10 +3,14 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+var screenRecordingDetector: ScreenRecordingDetector?
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+          screenRecordingDetector = ScreenRecordingDetector(messenger: controller.binaryMessenger)
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
